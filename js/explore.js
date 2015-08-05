6bd1ca706488c6ff95d2e778350c6f5d183e8d6c
Adam Mulligan <adammulligan@fastmail.fm>
2015-08-05 16:53:06 +0100

    Basic explore page

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+(function() {
+  var map = L.map('map').setView([0,-30], 3);
+
+  L.tileLayer('http://{s}.tiles.mapbox.com/v4/smbtc.7d2e3bf9/{z}/{x}/{y}@2x.png?access_token={access_token}', {
+    access_token: 'pk.eyJ1Ijoic21idGMiLCJhIjoiVXM4REppNCJ9.pjaLujYj-fcCPv5evG_0uA'
+  }).addTo(map);
+})();
