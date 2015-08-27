3897f09784778a00f2562b9ce90f2333014379d0
David Inga <david.inga@vizzuality.com>
2015-08-27 15:42:37 +0200

    new layout for planet pulse

---
+++ src/js/planet-pulse/globe.js
@@ -0,0 +1,174 @@
+var Globe = function(element) {
+  var webglEl = element || document.body;
+
+  // Vars
+  var w = element ? webglEl.clientWidth : window.innerWidth;
+  var h = element ? webglEl.clientHeight : window.innerHeight;
+
+  var radius = 0.5;
+  var segments = 32;
+  var rotation = 6;
+
+  var layers = {};
+
+  function createEarth(radius, segments) {
+    return new THREE.Mesh(
+      new THREE.SphereGeometry(radius, segments, segments),
+      new THREE.MeshPhongMaterial({
+        map: THREE.ImageUtils.loadTexture('/img/planet-pulse-layers/2_no_clouds_4k.jpg'),
+        bumpMap: THREE.ImageUtils.loadTexture('/img/planet-pulse-layers/elev_bump_4k.jpg'),
+        bumpScale: 0.0005,
+        specularMap: THREE.ImageUtils.loadTexture('/img/planet-pulse-layers/water_4k.png'),
+        specular: new THREE.Color('grey')
+      })
+    );
+  }
+
+  function createClouds(radius, segments) {
+    return new THREE.Mesh(
+      new THREE.SphereGeometry(radius + 0.003, segments, segments),     
+      new THREE.MeshPhongMaterial({
+        map: THREE.ImageUtils.loadTexture('/img/planet-pulse-layers/fair_clouds_4k.png'),
+        side: THREE.DoubleSide,
+        transparent: true,
+        depthWrite: false
+      })
+    );    
+  }
+
+  function createSpace(radius, segments) {
+    return new THREE.Mesh(
+      new THREE.SphereGeometry(radius, segments, segments),
+      new THREE.MeshBasicMaterial({
+        map: THREE.ImageUtils.loadTexture('/img/planet-pulse-layers/galaxy_starfield_4k.png'),
+        side: THREE.BackSide
+      })
+    );
+  }
+
+  function createLayer(name, imageUrl) {
+    var layer = new THREE.Mesh(
+      new THREE.SphereGeometry(radius + 0.004, segments, segments),     
+      new THREE.MeshPhongMaterial({
+        map: THREE.ImageUtils.loadTexture(imageUrl),
+        side: THREE.DoubleSide,
+        transparent: true,
+        depthWrite: false
+      })
+    );
+
+    // layer.rotation.y = rotation;
+    layers[name] = layer;
+    scene.add(layer);
+  }
+
+  function removeLayer(name) {
+    if (layers[name]) {
+      scene.remove(layers[name]);
+    }
+  }
+
+  // Scene
+  var scene = new THREE.Scene();
+
+  // Camera
+  var camera = new THREE.PerspectiveCamera(45, w / h, 0.01, 1000);
+  camera.position.z = 2;
+
+  // Renderer
+  var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setSize(w, h);
+
+  // Ambient light
+  var ambientLight = new THREE.AmbientLight(0x333333);
+  var light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.set(-5, 3, 5);
+
+  scene.add(ambientLight);
+  scene.add(light);
+
+  // Earth
+  var sphere = createEarth(radius, segments);
+  sphere.rotation.y = rotation;
+  scene.add(sphere);
+
+  // Clouds
+  var clouds = createClouds(radius, segments);
+  clouds.rotation.y = rotation;
+  scene.add(clouds);
+
+  // Space
+  var space = createSpace(90, 64);
+  scene.add(space);
+
+  // Controls
+  var controls = new THREE.TrackballControls(camera);
+
+  // Helper functions
+  function setPosition(x, y) {
+    if (x || x === 0) {
+      sphere.position.x = x;
+      clouds.position.x = x;
+    }
+
+    if (y || y === 0) {
+      sphere.position.y = y;
+      clouds.position.y = y;
+    }
+
+    for (l in layers) {
+      if (x || x === 0) {
+        layers[l].rotation.x = x;
+      }
+      if (y || y === 0) {
+        layers[l].rotation.y = y;
+      }
+    }
+  }
+
+  // Add element to DOM and render
+  webglEl.appendChild(renderer.domElement);
+
+  render();
+  setPosition(null, -0.1);
+
+  function render() {
+    controls.update();
+
+    sphere.rotation.y += 0.0005;
+    clouds.rotation.y += 0.0006;
+
+    for (l in layers) {
+      layers[l].rotation.y += 0.0005;
+    }
+
+    requestAnimationFrame(render);
+    renderer.clear();
+    renderer.render(scene, camera);
+  }
+
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+  }
+
+  window.addEventListener('resize', onWindowResize, false);
+
+  this.setPosition = setPosition;
+
+  this.addClouds = function() {
+    scene.add(clouds);
+  };
+
+  this.removeClouds = function() {
+    scene.remove(clouds);
+  };
+
+  this.resize = onWindowResize;
+
+  this.createLayer = createLayer;
+  this.removeLayer = removeLayer;
+
+  return this;
+}
