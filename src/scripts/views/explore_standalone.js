af5e1d04d715ddcd140284830b22eae56551493b
paulahaertel <paulahaertel@yahoo.de>
2015-09-21 13:39:38 +0200

    changes app section, card order, images for index.html. repairs data card dynamic linking. adds is-link class to data and insight cards.

--- src/scripts/views/explore_standalone.js
+++ src/scripts/views/explore_standalone.js
@@ -8,8 +8,10 @@ define([
 
   var ExploreView = Backbone.View.extend({
     initialize: function() {
-      // Sets the title to the url parameter title=""
+      // Sets the h2 to the url parameter "title"
       document.querySelectorAll('.explore--title')[0].innerHTML = window.location.search.split('?title=')[1].replace(/%20/g,' ');
+      //Sets head title to the url parameter "title"
+      document.getElementsByTagName('title')[0].innerHTML = window.location.search.split('?title=')[1].replace(/%20/g,' ');
     },
   });
 
