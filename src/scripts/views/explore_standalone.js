0391cb8c6cb4e15404a0bbfc8534302f566f0992
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2015-09-23 12:49:09 +0200

    Makes the explore-standalone page dynamic

--- src/scripts/views/explore_standalone.js
+++ src/scripts/views/explore_standalone.js
@@ -6,15 +6,68 @@ define([
 
   'use strict';
 
-  var ExploreView = Backbone.View.extend({
-    initialize: function() {
-      // Sets the h2 to the url parameter "title"
-      document.querySelectorAll('.explore--title')[0].innerHTML = window.location.search.split('?title=')[1].replace(/%20/g,' ');
-      //Sets head title to the url parameter "title"
-      document.getElementsByTagName('title')[0].innerHTML = window.location.search.split('?title=')[1].replace(/%20/g,' ');
+  var ExploreModel = Backbone.Model.extend({
+
+    initialize: function(options) {
+      this.id = options && options.id;
     },
+
+    url: function() {
+      return 'https://insights.cartodb.com/api/v2/sql?q=SELECT * FROM explore_table_config WHERE cartodb_id=' + this.id;
+    },
+
+    parse: function(data) {
+      return data && data.rows;
+    }
+
   });
 
-  return ExploreView;
+  var ExploreCollection = Backbone.Collection.extend({
+
+    url: 'https://insights.cartodb.com/api/v2/sql?q=SELECT * FROM explore_table_config',
+
+    parse: function(data) {
+      return data && data.rows;
+    }
+
+  });
+
+  var ExploreStandaloneView = Backbone.View.extend({
+
+    initialize: function() {
+      this.exploreModel = new ExploreModel({ id: window.location.hash.replace('#', '') });
+      this.exploreCollection = new ExploreCollection();
+
+      $.when.apply($, [this.exploreModel.fetch(), this.exploreCollection.fetch()])
+        .then(_.bind(this.render, this))
+        .fail(function() {
+          console.log('Error fetching the data');
+        });
+    },
+
+    render: function() {
+      var contentData = this.exploreModel.toJSON()[0];
+      var similarData = this.exploreCollection.toJSON();
+
+      var months = ['January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'];
+      var date = '-';
+      if(contentData.last_update) {
+        date = new Date(contentData.last_update);
+        date = months[date.getMonth()] + ', ' + date.getDate() + ' ' +
+          date.getFullYear();
+      }
+
+      $('.js-title').text(contentData.name);
+      $('.js-author').text(contentData.author);
+      $('.js-date').text(date);
+      $('.js-likes').toggleClass('is-active', contentData.is_liked);
+      $('.js-likes-count').text(contentData.likes_count);
+      $('.js-content').html(contentData.content);
+    }
+
+  });
+
+  return ExploreStandaloneView;
 
 });
